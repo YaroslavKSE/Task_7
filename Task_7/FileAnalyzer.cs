5eed938da3ce6bbf8ebb1b17743de9d650f529be
YaroslavKSE <ikhomych@kse.org.ua>
2022-05-11 22:51:07 +0300

    Added "FileAnalyzer" "FixPunctuation" "CheckForMistakes" functions

---
+++ Task_7/FileAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+namespace Task_7;
+
+public class FileAnalyzer
+{
+    private readonly string _path;
+    private readonly List<string> _data;
+
+    public FileAnalyzer(string path, List<string> data)
+    {
+        _path = path;
+        _data = data;
+    }
+
+    public void ReadFile()
+    {
+        foreach (var line in File.ReadAllLines(_path))
+        {
+            _data.Add(line);
+        }
+    }
+
+    public void CheckForMistakes(IList<string> wordsList)
+    {
+        Console.Write("Looks like you have typos in next words: ");
+        foreach (var word in wordsList)
+        {
+            if (!_data.Contains(word))
+            {
+                Console.Write($"'{word}' ");
+            }
+        }
+    }
+}
--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Task_7;
 
-Console.WriteLine("Hello, World!");
+var fileAnalyzer = new FileAnalyzer(@"D:\C#\Task_7\words_list.txt", new List<string>());
+fileAnalyzer.ReadFile();
+string? userInput = Console.ReadLine();
+var wordsList = userInput.Split(" ");
+FixPunctuation(wordsList);
+fileAnalyzer.CheckForMistakes(wordsList);
+
+void FixPunctuation(IList<string> data)
+{
+    for (var i = 0; i < data.Count; i++)
+    {
+        var word = data[i];
+        if (word.Contains(',') || word.Contains('.') || word.Contains('!') || word.Contains('?'))
+        {
+            var wordWithoutPunctuation = word.Substring(0, word.Length - 1);
+            data[i] = wordWithoutPunctuation;
+        }
+    }
+}
+
